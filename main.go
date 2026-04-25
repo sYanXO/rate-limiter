3ed47aa63c4592e4dac30ccced4661fea9661ada
sYanXO <sreayan69@gmail.com>
2026-04-25 12:03:02 +0530

    feat: added redis through lua scripting + tested

--- main.go
+++ main.go
@@ -1,79 +1,99 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"sync"
 	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
-// define the bucketState
+const luaRateLimitScript = `
+	local key = KEYS[1]
+	local max_tokens = tonumber(ARGV[1])
+	local refill_rate = tonumber(ARGV[2])
+	local now = tonumber(ARGV[3])
+	local requested = tonumber(ARGV[4])
 
-type Config struct { // separating concerns cuz these are global
-	MaxToken   float64
-	Refillrate float64
-}
-type TokenBucket struct {
-	Tokens         float64
-	LastRefillTime time.Time
-	Mu             sync.Mutex
-	Config         Config
+
+	local state = redis.call("HMGET",key,"tokens","last_refill")
+	local tokens = tonumber(state[1])
+	local last_refill = tonumber(state[2])
+
+
+	if tokens == nil then
+		tokens = max_tokens
+		last_refill = now
+
+	else
+		local elapsed = math.max(0, now - last_refill)
+		local refill = elapsed * refill_rate
+		tokens = math.min(max_tokens, tokens + refill)
+		last_refill = now
+	end
+
+	local allowed = 0
+	if tokens >= requested then
+		tokens = tokens - requested
+		allowed = 1
+	end
+
+	redis.call("HMSET",key,"tokens",tokens,"last_refill",last_refill)
+	redis.call("EXPIRE",key, math.ceil(max_tokens / refill_rate) + 60)
+
+	return allowed
+`
+
+type RedisRateLimiter struct {
+	client *redis.Client
+	script *redis.Script
 }
 
-func NewTokenBucket(config Config) *TokenBucket { // constructor
-	return &TokenBucket{
-		Tokens:         config.MaxToken,
-		LastRefillTime: time.Now(),
-		Config:         config,
+func NewRedisRateLimiter(client *redis.Client) *RedisRateLimiter {
+	return &RedisRateLimiter{
+		client: client,
+		script: redis.NewScript(luaRateLimitScript),
 	}
 }
 
-// refill func take in param as a tokenbucket
-func (tb *TokenBucket) refill() {
-	now := time.Now()
-	elapsed := now.Sub(tb.LastRefillTime).Seconds()
+func (rl *RedisRateLimiter) Allow(ctx context.Context, key string, maxTokens float64, refillRate float64, requested float64) (bool, error) {
+	now := float64(time.Now().Unix()) / 1e9 // unix gives in seconds but time.now is in nanoseconds, its a hacky way to fix this indifference
+	result, err := rl.script.Run(ctx, rl.client, []string{key}, maxTokens, refillRate, now, requested).Result()
 
-	refillTokens := elapsed * tb.Config.Refillrate
-
-	tb.Tokens += refillTokens
-
-	if tb.Tokens > tb.Config.MaxToken {
-		tb.Tokens = tb.Config.MaxToken
+	if err != nil {
+		return false, err
 	}
+	return result.(int64) == 1, nil
 
-	tb.LastRefillTime = now
 }
-
-// allow() func takes in how many req came in returns if they are allowed to pass or not
-func (tb *TokenBucket) allow(n float64) bool {
-	tb.Mu.Lock()
-	defer tb.Mu.Unlock()
-
-	tb.refill()
-
-	if tb.Tokens >= n {
-		tb.Tokens -= n
-		return true
-	}
-
-	return false
-}
-
 func main() {
-	fmt.Println("token bucket rate limiter demo")
+	fmt.Println("Distributed Redis Rate Limiter Demo : ")
 
-	tb := NewTokenBucket((Config{
-		MaxToken:   5, // play around
-		Refillrate: 1, // play around
-	}))
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
 
-	for i := 0; i < 25; i++ {
-		if tb.allow(1) {
-			fmt.Printf("Request %d : allowed\n", i+1)
-		} else {
-			fmt.Printf("Request %d : not allowed\n", i+1)
+	ctx := context.Background()
+
+	limiter := NewRedisRateLimiter(rdb)
+
+	userID := "user1" // in prod this maybe IP_address or API_token
+
+	for i := 0; i < 10; i++ {
+		// Max 5 tokens, refills 1 token per second, asking for 1 token
+		allowed, err := limiter.Allow(ctx, "ratelimit:"+userID, 5, 1, 1)
+		if err != nil {
+			fmt.Printf("Redis error : %v\n", err)
+			continue
 		}
 
-		time.Sleep(200 * time.Millisecond) // request rate
+		if allowed {
+			fmt.Printf("Request %d : allowed \n", i+1)
+		} else {
+			fmt.Printf("Request %d : blocked \n", i+1)
+		}
+
+		time.Sleep(200 * time.Millisecond)
 	}
 }
 
